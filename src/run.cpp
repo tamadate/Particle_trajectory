c891effefdd2625bf7c888a7e015fdc7218b2662
tama3rdGen <tamatomo.21@gmail.com>
2022-08-30 18:00:04 -0500

    implement OpenMP

--- src/run.cpp
+++ src/run.cpp
@@ -6,36 +6,39 @@ trajectory::run(void){
 	const int particleSize=vars->particles.size();
 	int trapParticle=0;	// Number of cases when the calculaiton is not finished in required time steps
 
-	# pragma omp parallel for
-	for (int pid=0; pid<particleSize; pid++){
-		int nth=omp_get_thread_num();
-		particle &a=vars->particles[pid];
-		initialize(a,nth);
+	# pragma omp parallel
+	{
+		# pragma omp for
+		for (int pid=0; pid<particleSize; pid++){
+			int nth=omp_get_thread_num();
+			particle &a=vars->particles[pid];
+			initialize(a,nth);
 
-		int breakFlag=0;
-		double preOutTime=0;
-		double time=0;
-		while(time<totalTime){
-			if(time - preOutTime > observeTime) {
-				output(a, time);
-				preOutTime=time;
+			int breakFlag=0;
+			double preOutTime=0;
+			double time=0;
+			while(time<totalTime){
+				if(time - preOutTime > observeTime) {
+					output(a, time);
+					preOutTime=time;
+				}
+
+				time+=timeEvolution(a);
+
+				breakFlag=checkCell(pid);
+				if(breakFlag==-1) break;
+
+				if(a.update==1) updateDisp(a);	// Update dispersion?
 			}
 
-			time+=timeEvolution(a);
-
-			breakFlag=checkCell(pid);
-			if(breakFlag==-1) break;
-
-			if(a.update==1) updateDisp(a);	// Update dispersion?
-		}
-
-		// if particle did not hit on any "bounday" during wall time
-		if(breakFlag==0){
-			outParticles[pid].pid=a.id;
-			outParticles[pid].r=a.x;
-			outParticles[pid].v=a.v;
-			outParticles[pid].bid=-1;
-			//trapParticle++;
+			// if particle did not hit on any "bounday" during wall time
+			if(breakFlag==0){
+				outParticles[pid].pid=a.id;
+				outParticles[pid].r=a.x;
+				outParticles[pid].v=a.v;
+				outParticles[pid].bid=-1;
+				//trapParticle++;
+			}
 		}
 	}
 
