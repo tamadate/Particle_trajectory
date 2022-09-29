eb39aaf6f1820a7235ff97a1f27fe78d481b57c9
tama3rdGen <tamatomo.21@gmail.com>
2022-09-28 19:25:39 -0500

    Add tools

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 
 int main(int argc,char *argv[]){
-	trajectory *tra = new trajectory();		// trajectory class is initialized with constractor (see trajectory.cpp)
+	trajectory *tra = new trajectory( );		// trajectory class is initialized with constractor (see trajectory.cpp)
 	if (tra->flags->initialBreakFlag==0) tra->run();  // This is main part of this simulaiton
 	return 0;
 }
---
+++ tool/postProcessing/FinalLocation.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+##  To organize figure style
+def pltNormal():
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['figure.subplot.bottom'] = 0.15
+    plt.rcParams['figure.subplot.left'] = 0.2
+    plt.rcParams["font.size"]=10
+
+def axNormal(ax):
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.tick_params(axis='x')
+    ax.tick_params(axis='y')
+
+
+pltNormal()
+fig, axs = plt.subplots(1,1,figsize=(7,5))
+axNormal(axs)
+
+center=np.array((0,0,0))    # center of calculation domain
+directory="/home/tama3rdgen/jetaxis/NewMesh/100nm/"
+data=np.loadtxt(directory+"finalPosition.dat")
+
+mappingInt=100
+rout=0.05
+iX=1
+iY=2
+saveFigure=0
+
+delta=rout*2/float(mappingInt)
+x=np.arange(0.5*delta,mappingInt*delta,delta)-rout
+dist=np.zeros((mappingInt,mappingInt))
+Iout=np.sum(data.T[4]==5)
+Itot=np.sum(data.T[4])
+
+P=Iout/float(Itot)
+
+for i in data:
+	if(i[4]==5):
+		ix=int((i[iX]-center[iY]+rout)/delta)
+		iy=int((i[iX]-center[iY]+rout)/delta)
+		dist[ix][iy]+=1
+
+rout*=1e3
+X,Y=np.meshgrid(x,x)
+axs.set_xlabel(r'$x$ [mm]',size=12)
+axs.set_ylabel(r'$y$ [mm]',size=12)
+axs.set_xlim(-rout,rout)
+axs.set_ylim(-rout,rout)
+pl=axs.contourf(X*1e3,Y*1e3,dist/np.max(dist),levels=np.linspace(0,1,100),cmap="gray")
+axs.set_title("Penetration: {:.01f} %".format(P*100),size=15)
+plt.colorbar(pl,ticks=np.array([0,0.2,0.4,0.6,0.8,1.0])).set_label(label="Normalized particle concentration [-]",size=12)
+
+if(saveFigure):
+    plt.savefig(directory+"location.png", dpi=1000)
+plt.show()
